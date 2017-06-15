e105648f8ae050d63bf6fef49753923231291e59
Thomas Chen <chen.thomas.c@gmail.com>
2017-06-15 02:48:17 -0400

    Implement GenServer WIP

---
+++ lib/incrementer/handler.ex
@@ -0,0 +1,19 @@
+defmodule Incrementer.Handler do
+  use GenServer
+
+  def start(key) do
+    GenServer.start_link(__MODULE__, 0, name: key)
+  end
+
+  def increment(pid, value) do
+    GenServer.call(pid, {:increment, value})
+  end
+
+  # Callbacks
+
+  def handle_call({:increment, value}, _from, state) do
+    new_state = state + value
+
+    {:reply, new_state, new_state}
+  end
+end
--- lib/incrementer/router.ex
+++ lib/incrementer/router.ex
@@ -1,31 +1,40 @@
-defmodule MoodRingSup.Router do
+defmodule Incrementer.Router do
   use Plug.Router
-  require Plug.Conn.Query
 
+  plug Plug.Parsers, parsers: [:urlencoded]
   plug :match
   plug :dispatch
 
   get "/" do
     conn
-    |> send_resp(200, "wow!")
+    |> send_resp(200, "get!")
   end
 
   post "/increment" do
-    {:ok, body, _} = read_body(conn);
-    %{"key" => key, "value" => value} = Plug.Conn.Query.decode(body)
-    IO.inspect(key)
-    IO.inspect(String.to_integer(value))
+    %{"key" => key, "value" => value} = conn.params
+    key = String.to_atom(key)
+    value = String.to_integer(value)
+
+    pid = case GenServer.whereis(key) do
+      nil ->
+        {:ok, process} = Incrementer.Handler.start(key)
+        process
+      _ ->
+        key
+    end
+
+    response = Incrementer.Handler.increment(pid, value)
 
     conn
-    |> send_resp(200, 'success!')
+    |> send_resp(200, Integer.to_string(response))
   end
 
   match _ do
     conn
-    |> send_resp(200, "woooow!")
+    |> send_resp(200, "other!")
   end
 
   def start_link do
-    {:ok, _} = Plug.Adapters.Cowboy.http(MoodRingSup.Router, [], port: 3333)
+    {:ok, _} = Plug.Adapters.Cowboy.http(Incrementer.Router, [], port: 3333)
   end
 end
