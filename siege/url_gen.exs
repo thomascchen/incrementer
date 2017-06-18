db5607e7c58e96cfde40f713b027784ceca38ad7
Thomas Chen <chen.thomas.c@gmail.com>
2017-06-17 23:41:04 -0400

    Generate load test data for siege

---
+++ siege/url_gen.exs
@@ -0,0 +1,19 @@
+defmodule Siege.UrlGen do
+  def generate do
+    rows = Enum.map(
+      1..1_000,
+      fn(_) -> construct() end
+    )
+
+    joined_rows = Enum.join(rows, "\n")
+
+    File.write('./siege/urls.txt', joined_rows)
+  end
+
+  defp construct do
+    key = Enum.random(1..1_000)
+    value = Enum.random(1..1_000)
+
+    "http://localhost:3333/increment POST key=#{key}&value=#{value}"
+  end
+end
