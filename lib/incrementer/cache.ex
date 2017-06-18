c6081decff48d92b60a14d048a4284562c908e8f
Thomas Chen <chen.thomas.c@gmail.com>
2017-06-18 01:52:15 -0400

    Remove log_limit

--- lib/incrementer/cache.ex
+++ lib/incrementer/cache.ex
@@ -3,18 +3,17 @@ defmodule Incrementer.Cache do
 
   def start_link(opts \\ []) do
     GenServer.start_link(__MODULE__, [
-      {:ets_table_name, :cache},
-      {:log_limit, 1_000_000}
+      {:ets_table_name, :cache}
     ], opts)
   end
 
   # Callbacks
 
   def init(args) do
-    [{:ets_table_name, ets_table_name}, {:log_limit, log_limit}] = args
+    [{:ets_table_name, ets_table_name}] = args
 
     :ets.new(ets_table_name, [:named_table, :set, :public, {:write_concurrency, true}])
 
-    {:ok, %{log_limit: log_limit, ets_table_name: ets_table_name}}
+    {:ok, %{ets_table_name: ets_table_name}}
   end
 end
